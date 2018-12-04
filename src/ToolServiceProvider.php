49c5f84fecffa00aea94e6b25b2dc134a0ba225e
Chris Ware <chris.ware93@yahoo.com>
2018-12-04 20:36:05 +0000

    Apply fixes from StyleCI

--- src/ToolServiceProvider.php
+++ src/ToolServiceProvider.php
@@ -2,11 +2,7 @@
 
 namespace ChrisWare\NovaBreadcrumbs;
 
-use Laravel\Nova\Nova;
-use Laravel\Nova\Events\ServingNova;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
-use ChrisWare\NovaBreadcrumbs\Http\Middleware\Authorize;
 
 class ToolServiceProvider extends ServiceProvider
 {
