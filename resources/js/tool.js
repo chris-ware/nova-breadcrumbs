bf3d411e94ddbbad591ec239e8e3a6ac1026e4ed
gaabora <gaabora@gmail.com>
2019-08-07 14:30:38 +0300

    remove unnecessery code caused 'duplicate route' warnings
    2.0.9 all ok, maybe need to be tested on other versions

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,9 +1,3 @@
-import CustomAttach from '@local/views/CustomAttach';
-import CustomCreate from '@local/views/CustomCreate';
-import CustomDashboard from '@local/views/CustomDashboard';
-import CustomIndex from '@local/views/CustomIndex';
-import CustomUpdate from '@local/views/CustomUpdate';
-import CustomUpdateAttached from '@local/views/CustomUpdateAttached';
 
 Nova.booting((Vue, router) => {
 
@@ -35,76 +29,4 @@ Nova.booting((Vue, router) => {
             next();
         }
     });
-
-    router.addRoutes([
-        {
-            name: 'custom-dashboard',
-            path: '/',
-            component: CustomDashboard,
-            props: true
-        },
-        {
-            name: 'custom-index',
-            path: '/resources/:resourceName',
-            component: CustomIndex,
-            props: true
-        },
-        {
-            name: 'custom-create',
-            path: '/resources/:resourceName/new',
-            component: CustomCreate,
-            props: route => {
-                return {
-                    resourceName: route.params.resourceName,
-                    viaResource: route.query.viaResource,
-                    viaResourceId: route.query.viaResourceId,
-                    viaRelationship: route.query.viaRelationship,
-                }
-            },
-        },
-        {
-            name: 'custom-edit',
-            path: '/resources/:resourceName/:resourceId/edit',
-            component: CustomUpdate,
-            props: function props(route) {
-                return {
-                    component: route.params.component,
-                    resourceName: route.params.resourceName,
-                    resourceId: route.params.resourceId,
-                    viaResource: route.query.viaResource,
-                    viaResourceId: route.query.viaResourceId,
-                    viaRelationship: route.query.viaRelationship
-                };
-            }
-        }, {
-            name: 'custom-attach',
-            path: '/resources/:resourceName/:resourceId/attach/:relatedResourceName',
-            component: CustomAttach,
-            props: function props(route) {
-                return {
-                    component: route.params.component,
-                    resourceName: route.params.resourceName,
-                    resourceId: route.params.resourceId,
-                    relatedResourceName: route.params.relatedResourceName,
-                    viaRelationship: route.query.viaRelationship,
-                    polymorphic: route.query.polymorphic == '1'
-                };
-            }
-        }, {
-            name: 'custom-edit-attached',
-            path: '/resources/:resourceName/:resourceId/edit-attached/:relatedResourceName/:relatedResourceId',
-            component: CustomUpdateAttached,
-            props: function props(route) {
-                return {
-                    component: route.params.component,
-                    resourceName: route.params.resourceName,
-                    resourceId: route.params.resourceId,
-                    relatedResourceName: route.params.relatedResourceName,
-                    relatedResourceId: route.params.relatedResourceId,
-                    viaRelationship: route.query.viaRelationship
-                };
-            }
-        }
-    ]);
-
 })
